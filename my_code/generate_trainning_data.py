cd41f91fdaf0f9afb177640d6700195a7c6f53a8
sklaw <sk.law.lsq@gmail.com>
2016-09-09 03:37:51 +0800

    BIG FIX: success&fail samples should have the same dimension; all state samples should have the same dimension.

--- my_code/generate_trainning_data.py
+++ my_code/generate_trainning_data.py
@@ -22,8 +22,7 @@ DB_PRINT=0
 
 def main():
     ## Flags
-    successFlag=1
-    failureFlag=1
+    successAndFailFlag=1
     hlStatesFlag=1
     output_per_one_trial_flag=1 # if true, output is performed for each trial for all axis. Otherwise all trials for each axis is displayed.
 
@@ -84,7 +83,7 @@ def main():
 
     # 1. Get data for success tasks for a given success_strategy
 
-    if successFlag:
+    if successAndFailFlag:
         strategy=success_strategy    
         file_for_success_set = open(os.path.join(base_dir, '..', 'my_training_data', strategy, 'training_set_of_success'), 'w')
         
@@ -99,14 +98,7 @@ def main():
             if data_folder_name[:2] == '20':
                 data_folder_names.append(data_folder_name)
                 
-        numTrials=len(data_folder_names)
         
-        # Get max number of iterations for each level/axis for entire experiment sets      
-        # Construct dictionary to hold dimensions. Need level and axis sub-spaces.
-        for level in levels:
-            folder_dims[level] = {}
-            for axis in axes:
-                folder_dims[level][axis]=0
         
         # Create a dictionary structure for all trials, RCBHT levels, and axis.
         for data_folder_name in data_folder_names:
@@ -120,41 +112,17 @@ def main():
             else:
                 dict_all[data_folder_name]=dict_cooked_from_folder
 
+
+
+
+
+
+
+
         if bool(dict_all):
-            # Once dict_cooked_from_folder exists, get dimensions of level/axis for each folder
-            for data_folder_name in data_folder_names:
-                for level in levels:
-                    for axis in axes: 
-                        folder_dims[level][axis] = len(dict_all[data_folder_name][level][axis])
-                dict_dims[data_folder_name]=deepcopy(folder_dims)
-                        
-            if bool(dict_dims):
-                # Only keep the largest dimensions for each level/axis
-                for level in levels:
-                    for axis in axes:                            
-                        for data_folder_name in data_folder_names:                
-                            temp=dict_dims[data_folder_name][level][axis] 
-                            if temp > folder_dims[level][axis]:
-                                folder_dims[level][axis]=temp
-            
-                # For each trial, take the dictionary and reshape it (change number of iterations), output is the same NUMBER of labels for all axes.             
-                for data_folder_name in data_folder_names:        
-                    data_feature_extractor.extract_features(dict_all[data_folder_name],folder_dims)
-                    
-                    # Create the allTrialsLables structure. It is organized by: state/trials/level/axis. Only contains labels.
-                    allTrialLabels[data_folder_name]=deepcopy(dict_all[data_folder_name])  
-            
-                        
-                # label s indicates SUCCESS. Have a file and a place to put images
-                if output_per_one_trial_flag:
-                    output_features.output_sample_one_trial(file_for_success_set, 's', allTrialLabels)
-                else:
-                    output_features.output_sample_all_trial(file_for_success_set, 's', allTrialLabels,data_folder_names,numTrials,os.path.join(base_dir, '..', 'my_training_data',strategy))
-                
-            else:
-                print 'The success dictionary dict_dims is empty'
+            success_dict_all = dict_all;
         else:
-            print 'The success dictionary dict_all is empty' 
+            raise Exception('The success dictionary dict_all is empty') 
         
         # Clear up
         folder_dims={}
@@ -168,7 +136,6 @@ def main():
     ##FAILURE ANALYSIS
     #------------------------------------------------------------------------_
         
-    if failureFlag:   
         strategy=failure_strategy
         file_for_fail_set = open(os.path.join(base_dir, '..', 'my_training_data', strategy, 'training_set_of_fail'), 'w')    
         
@@ -181,14 +148,6 @@ def main():
             if data_folder_name[:2] == 'ex' or data_folder_name[:2] == 'FC':
                 failure_data_folder_names.append(data_folder_name)   
                 
-        numTrials=len(failure_data_folder_names)
-                
-        # Get max number of iterations for each level/axis for entire experiment sets      
-        # Construct dictionary to hold dimensions. Need level and axis sub-spaces.
-        for level in levels:
-            folder_dims[level] = {}
-            for axis in axes:
-                folder_dims[level][axis]=0
         
         # Get full path for each folder name
         for data_folder_name in failure_data_folder_names:
@@ -205,41 +164,9 @@ def main():
 
         # Once dict_cooked_from_folder exists, get dimensions of level/axis for each folder
         if bool(dict_all):        
-            for data_folder_name in failure_data_folder_names:
-                for level in levels:
-                    for axis in axes: 
-                        folder_dims[level][axis] = len(dict_all[data_folder_name][level][axis])
-                dict_dims[data_folder_name]=deepcopy(folder_dims)
-                    
-            if bool(dict_dims):
-                # Only keep the largest dimensions for each level/axis
-                for level in levels:
-                    for axis in axes:                            
-                        for data_folder_name in failure_data_folder_names:                
-                            temp=dict_dims[data_folder_name][level][axis] 
-                            if temp > folder_dims[level][axis]:
-                                folder_dims[level][axis]=temp
-                    
-                # For one trial, take the dictionary and reshape it (change number of iterations) so we have the same NUMBER of labels for all axes. 
-                # Then only return the labels.
-                # Currently we take the max number of iterations in any given trial/level/axis.
-                for data_folder_name in failure_data_folder_names:        
-                    data_feature_extractor.extract_features(dict_all[data_folder_name],folder_dims)
-                    allTrialLabels[data_folder_name]=deepcopy(dict_all[data_folder_name]) 
-                
-                    
-                
-                # Write labels and images to file. 2 choices: individual iterations or all iterations per state.            
-                # label f indicates SUCCESS. Have a file and a place to put images
-                if output_per_one_trial_flag:
-                    output_features.output_sample_one_trial(file_for_fail_set, 'f', allTrialLabels); 
-                else:
-                    output_features.output_sample_all_trial(file_for_fail_set, 'f', allTrialLabels,failure_data_folder_names, numTrials,os.path.join(base_dir, '..', 'my_training_data',failure_strategy))
-                
-            else:
-                print 'The failure dictionary dict_dims is empty'
+            fail_dict_all = dict_all;
         else:
-            print 'The failure dictionary dict_all is empty'
+            raise Exception('The failure dictionary dict_all is empty')
 
         # Clear up
         folder_dims={}
@@ -249,6 +176,52 @@ def main():
         data_folder_names=[]        
         orig_data_folder_names=[]
         failure_data_folder_names=[]
+
+        for level in levels:
+            folder_dims[level] = {}
+            for axis in axes:
+                folder_dims[level][axis]=0
+
+        #cook folder_dims for both success&fail samples
+        for dict_all in [success_dict_all, fail_dict_all]:
+            for data_folder_name in dict_all:
+                for level in levels:
+                    for axis in axes: 
+                        temp = len(dict_all[data_folder_name][level][axis])
+                        if temp > folder_dims[level][axis]:
+                            folder_dims[level][axis] = temp
+
+        #output data for success
+        dict_all = success_dict_all
+        for data_folder_name in dict_all:        
+            data_feature_extractor.extract_features(dict_all[data_folder_name],folder_dims)
+            allTrialLabels[data_folder_name]=deepcopy(dict_all[data_folder_name])  
+        if output_per_one_trial_flag:
+            output_features.output_sample_one_trial(file_for_success_set, 's', allTrialLabels)
+        else:
+            output_features.output_sample_all_trial(file_for_success_set, 's', allTrialLabels, allTrialLabels.keys(), len(allTrialLabels), os.path.join(base_dir, '..', 'my_training_data',success_strategy))
+
+        #output data for fail
+        dict_all = fail_dict_all
+        for data_folder_name in dict_all:        
+            data_feature_extractor.extract_features(dict_all[data_folder_name],folder_dims)
+            allTrialLabels[data_folder_name]=deepcopy(dict_all[data_folder_name])  
+        if output_per_one_trial_flag:
+            output_features.output_sample_one_trial(file_for_fail_set, 'f', allTrialLabels); 
+        else:
+            output_features.output_sample_all_trial(file_for_fail_set, 'f', allTrialLabels, allTrialLabels.keys(), len(allTrialLabels), os.path.join(base_dir, '..', 'my_training_data',failure_strategy))
+
+        # Clear up
+        folder_dims={}
+        dict_dims={}
+        dict_all={}
+        allTrialLabels={}
+        data_folder_names=[]        
+        orig_data_folder_names=[]
+        failure_data_folder_names=[]
+
+
+
     #-------------------------------------------------------------------------
     ## Parse information by State 
     #-------------------------------------------------------------------------
@@ -273,15 +246,7 @@ def main():
                 
         numTrials=len(data_folder_names)
                 
-        # Get max number of iterations for each level/axis for entire experiment sets      
-        # Construct dictionary to hold dimensions. Need level and axis sub-spaces.
-        for state in states:
-            folder_dims[state] = {}
-            for level in levels:
-                folder_dims[state][level] = {}
-                for axis in axes:
-                    folder_dims[state][level][axis]=0           
-                
+               
         # Create a dictionary structure for all trials, RCBHT levels, and axis.
         for data_folder_name in data_folder_names:
             data_folder_full_path = os.path.join(data_folder_prefix, data_folder_name)
@@ -297,58 +262,50 @@ def main():
             
         # Generate a dictionary of dimensions for all trials/states/levels/axis
         if bool(dict_all): 
-            for data_folder_name in data_folder_names:
+            folder_dims = {}
+            for level in levels:
+                folder_dims[level] = {}
+                for axis in axes:
+                    folder_dims[level][axis]=0           
+         
+            for data_folder_name in dict_all:
                 for state in states:
                     for level in levels: 
                         for axis in axes:
-                            folder_dims[state][level][axis] = len(dict_all[data_folder_name][state][level][axis])
+                            temp = len(dict_all[data_folder_name][state][level][axis])
+                            if temp > folder_dims[level][axis]:
+                                folder_dims[level][axis]=temp
                             
-                dict_dims[data_folder_name]=deepcopy(folder_dims)
-            # Only keep the largest dimensions for each state/level/axis
-            if bool(dict_dims):     
-                for data_folder_name in data_folder_names:
-                    for state in states:
-                        for level in levels:
-                            for axis in axes:                                                                        
-                                temp=dict_dims[data_folder_name][state][level][axis] 
-                                if temp > folder_dims[state][level][axis]:
-                                    folder_dims[state][level][axis]=temp
                                     
-                # For one trial, take the dictionary and reshape it (change number of iterations) so we have the same NUMBER of labels for all axes. 
-                # Then only return the labels.
-                # Currently we take the max number of iterations in any given trial/level/axis.
-                allTrialLabels={}
-                for data_folder_name in data_folder_names:
-                    for state in states:                
-                        data_feature_extractor.extract_features(dict_all[data_folder_name][state], folder_dims[state])                                                                                          
-                                        
+            # For one trial, take the dictionary and reshape it (change number of iterations) so we have the same NUMBER of labels for all axes. 
+            # Then only return the labels.
+            # Currently we take the max number of iterations in any given trial/level/axis.
+            allTrialLabels={}
+            for data_folder_name in data_folder_names:
+                for state in states:                
+                    data_feature_extractor.extract_features(dict_all[data_folder_name][state], folder_dims)                                                                                          
                 
-                    
-                # Create the allTrialsLables structure. It is organized by: state/trials/level/axis. Only contains labels.
-                for state in states:
-                    allTrialLabels[state]={}
-                    for data_folder_name in data_folder_names:
-                        allTrialLabels[state][data_folder_name]={}                        
-                        allTrialLabels[state][data_folder_name]=deepcopy(dict_all[data_folder_name][state])                        
-                            
-                ## Create directories to keep both labels and images in file
-                for state in states:
-                    hlb_dir=strategy + '/' + 'img_of_'+state
-                    if not os.path.exists(os.path.join(base_dir, '..', 'my_training_data', hlb_dir)):
-                        os.makedirs(os.path.join(base_dir, '..', 'my_training_data', hlb_dir))
-                    
-                    # Write labels and images to file. 2 choices: individual iterations or all iterations per state.
-                    # label 1 indicates SUCCESS. Have a file and a place to put images
-                    if output_per_one_trial_flag:
-                        output_features.output_sample_one_trial(files_for_states[state], state, allTrialLabels[state])
-                    else:
-                        output_features.output_sample_all_trial(files_for_states[state], state, allTrialLabels[state],data_folder_names,numTrials,os.path.join(base_dir, '..', 'my_training_data',hlb_dir))
-            
-                    
-            else:
-                print 'The hlb dictionary dict_dims is empty'
+            # Create the allTrialsLables structure. It is organized by: state/trials/level/axis. Only contains labels.
+            for state in states:
+                allTrialLabels[state]={}
+                for data_folder_name in data_folder_names:
+                    allTrialLabels[state][data_folder_name]={}                        
+                    allTrialLabels[state][data_folder_name]=deepcopy(dict_all[data_folder_name][state])                        
+                        
+            ## Create directories to keep both labels and images in file
+            for state in states:
+                hlb_dir=strategy + '/' + 'img_of_'+state
+                if not os.path.exists(os.path.join(base_dir, '..', 'my_training_data', hlb_dir)):
+                    os.makedirs(os.path.join(base_dir, '..', 'my_training_data', hlb_dir))
+                
+                # Write labels and images to file. 2 choices: individual iterations or all iterations per state.
+                # label 1 indicates SUCCESS. Have a file and a place to put images
+                if output_per_one_trial_flag:
+                    output_features.output_sample_one_trial(files_for_states[state], state, allTrialLabels[state])
+                else:
+                    output_features.output_sample_all_trial(files_for_states[state], state, allTrialLabels[state], data_folder_names, numTrials, os.path.join(base_dir, '..', 'my_training_data',hlb_dir))
         else:
-                print 'dict_all from hlb states is not available'
+                raise Exception('dict_all from hlb states is not available')
         folder_dims={}
         dict_dims={}
         dict_all={}
